bb5e0feb0b164b7da2dc1311a868b427b561145e
Ricardo Pineda <richay718@gmail.com>
2019-09-10 14:00:33 -0400

    first card and column added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TrelloList from './TrelloList';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi!</h1>
+      <TrelloList title="First List" />
     </div>
   );
 }
---
+++ src/components/TrelloCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+
+const TrelloCard = () => {
+  return (
+    <Card>
+      <Typography gutterBottom>Word of the Day</Typography>
+    </Card>
+  );
+};
+
+export default TrelloCard;
